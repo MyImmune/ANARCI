f1af4e0ca49468d97be9d98def1f3fe1f77f6258
Ayan Sengupta <ayansengupta17@gmail.com>
2026-04-09 19:02:52 +0900

    distutils was removed in Python 3.12.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import shutil, os
 import site, sys
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 from setuptools.command.install import install
 
 
